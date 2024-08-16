9c188bf3e5c5e217b83aceb1661e5b4ec803679f
7Fin7 <finley.leggett@gmail.com>
2024-08-16 13:46:59 +0200

    base

---
+++ src/BasicCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Basic Java Calculator");
+    }
+
+    public static double addNumbers(double num1,double num2) {
+        return 0.0;
+    }
+
+    public static double subtractNumbers(double num1,double num2) {
+        return 0.0;
+    }
+
+    public static double multiplyNumbers(double num1,double num2) {
+        return 0.0;
+    }
+
+    public static double divideNumbers(double num1,double num2) {
+        return 0.0;
+    }
+}
